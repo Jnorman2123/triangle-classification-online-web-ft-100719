078f1c364ba42847441f8c41d1a1fccafc9cf617
Jonathan Norman <jnorman2123@gmail.com>
2019-10-24 16:43:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
 
   def kind
     @type
-    if (@a <= 0 || @b <= 0 || @c <= 0)
+    if (@a <= 0 || @b <= 0 || @c <= 0) || (@a + @b <= @c) || (@a + @c <= @b) || (@b + @c <= @a)
       begin
         raise TriangleError
         puts error.message
